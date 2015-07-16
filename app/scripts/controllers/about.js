864d935187c715485b287303cf1a06efdfc31f51
Courtney Mitchell <courtney.mitchell@live.com>
2015-07-15 18:23:27 -0700

    scaffold angular app

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name blocGoalsAppApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the blocGoalsAppApp
+ */
+angular.module('blocGoalsAppApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name blocGoalsAppApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the blocGoalsAppApp
+ */
+angular.module('blocGoalsAppApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
