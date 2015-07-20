e74d9e6ee798b9665ad3f388396f3bc40cf831f0
Courtney Mitchell <courtney.mitchell@live.com>
2015-07-20 10:32:42 -0700

    integrate with rails API using restangular. display list of goals from api on main page

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,7 @@
  * Controller of the blocGoalsAppApp
  */
 angular.module('blocGoalsAppApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function ($scope, Restangular) {
+    Restangular.setBaseUrl('http://127.0.0.1:3000');
+    $scope.goals = Restangular.all('goals').getList().$object;
   });
